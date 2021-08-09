8b7275a5183907fbc378eeab1f349a05f44353b6
Vasanth Kumar <vasanth.kumar1@byjus.com>
2021-08-09 16:15:01 +0530

    modified

--- src/BootScene.js
+++ src/BootScene.js
@@ -1,7 +1,9 @@
 import Phaser from "phaser";
-import logo from "./assets/logo.png";
-import red from "./assets/red.png";
-import space from "./assets/space.png";
+// import logo from "./assets/logo.png";
+// import red from "./assets/red.png";
+// import space from "./assets/space.png";
+
+import images from './assets/*.png';
 
 export default class BootScene extends Phaser.Scene {
   constructor() {
@@ -16,9 +18,9 @@ export default class BootScene extends Phaser.Scene {
 
     // console.table(images);
 
-    this.load.image("space", space);
-    this.load.image("logo", logo);
-    this.load.image("red", red);
+    this.load.image("space", images.space);
+    this.load.image("logo", images.logo);
+    this.load.image("red", images.red);
 
     this.load.on("progress", function (progress) {
       bar.setScale(progress, 1);
